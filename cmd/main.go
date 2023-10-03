de85cf6579c863aadfb2eb8b0c7e43c4019ccbe1
Hudsn <70234973+Hudsn@users.noreply.github.com>
2023-10-02 23:58:34 -0700

    implement iframe approach

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,22 +12,43 @@ import (
 
 func main() {
 
-	tepidConfig := tepidreload.NewConfig(tepidreload.WithWatchPath("test_static"))
+	watchPath := flag.String("path", "./", "relative path to directory to monitor for reloads")
+	flag.Parse()
+
+	tepidConfig := tepidreload.NewConfig(tepidreload.WithWatchPath(*watchPath))
+
 	script, checker := tepidreload.MakeHandlers("/tepid", tepidConfig)
 
 	r := chi.NewRouter()
+
+	staticHandler, ok := http.StripPrefix("/tepidstatic/", serveStatic(*watchPath)).(http.HandlerFunc)
+	if !ok {
+		log.Fatal("Unable to initialize static handler.")
+	}
+
 	r.Get("/tepid.js", script)
 	r.Get("/tepid", checker)
-	r.Get("/", normal)
+	r.Get("/tepidstatic/*", staticHandler)
+	r.Get("/*", tepidreload.IframeHandler())
 
 	fmt.Println("Starting the server on :3000")
+	fmt.Println("Serving files from ", *watchPath)
 	http.ListenAndServe(":3000", r)
 }
 
-func normal(w http.ResponseWriter, r *http.Request) {
-	r.Header.Set("Content-Type", "text/html")
-	fmt.Fprint(w, `
-	<h1>Hello world</h1>
-	<script src="/tepid.js"></script>
-	`)
+func serveStatic(filepath string) http.HandlerFunc {
+
+	dir := http.Dir(filepath)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// perma-nuke cache so we don't get weird interactions if the user changes the watched directory between uses.
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Add("Cache-Control", "no-store")
+		w.Header().Add("Cache-Control", "must-revalidate")
+		w.Header().Set("Expires", "0")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Del("Last-Modified")
+		http.FileServer(dir).ServeHTTP(w, r)
+	}
+
 }
